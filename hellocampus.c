d20a1e30253dddc0e8aafbf68febee1f90337db8
Viita <ysy825149732@gmail.com>
2025-05-25 01:04:46 +0800

    helloworld in different language

---
+++ hellocampus.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello, New campus life!\n");
+  return 0;
+}
--- main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Hello, New campus life!\n");
-    return 0;
-}
