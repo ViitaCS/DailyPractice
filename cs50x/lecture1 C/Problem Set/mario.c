a0871f56dc305519bbe75fdbcc02d525773d8aae
Viita <ysy825149732@gmail.com>
2025-05-27 00:01:48 +0800

    hello

---
+++ cs50x/lecture1 C/Problem Set/mario.c
@@ -0,0 +1,38 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print(int cur, int height);
+
+int main(void)
+{
+    int h;
+    do
+    {
+        h = get_int("Height: ");
+    }
+    while (h < 1 || h > 8); // 输入检查
+
+    for (int i = 0; i < h; i++) // 决定打印的行数
+    {
+        print(i, h);
+    }
+}
+
+// 根据高度逐行打印
+void print(int cur, int height)
+{
+    for (int i = height; i > cur + 1; i--)
+    {
+        printf(" ");
+    }
+    for (int i = 0; i <= cur; i++)
+    {
+        printf("#");
+    }
+    printf("  ");
+    for (int i = 0; i <= cur; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
