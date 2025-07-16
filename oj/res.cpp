e76d193b145e203576122679335ae334f87026bd
Viita <ysy825149732@gmail.com>
2025-07-16 14:55:29 +0800

    2025.7.16

--- oj/res.cpp
+++ oj/res.cpp
@@ -1,4 +1,3 @@
-#include <cstddef>
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -12,29 +11,36 @@ struct ListNode {
 };
 class Solution {
 public:
-  vector<ListNode *> splitListToParts(ListNode *root, int k) {
-    int count = 0;
-    ListNode *cur = root;
-    while (cur) {
-      count++;
-      cur = cur->next;
-    }
-
-    int m = count / k;
-    int n = count % k;
-    vector<ListNode *> result(k, nullptr);
-    cur = root;
-    for (int i = 0; i < k && cur; i++) {
-      result[i] = cur;
-      int ex = m + (i < n ? 1 : 0);
-      for (int j = 0; j < ex - 1; j++) {
-        cur = cur->next;
+  ListNode *reverseKGroup(ListNode *head, int k) {
+    if (k == 1 || !head)
+      return head;
+    ListNode *dummy = new ListNode(0);
+    dummy->next = head;
+    ListNode *preGroupEnd = dummy;
+    while (true) {
+      ListNode *curr = preGroupEnd->next;
+      ListNode *groupStart = curr;
+      int count = 0;
+      while (curr && count < k) {
+        curr = curr->next;
+        count++;
       }
-      ListNode *next = cur->next;
-      cur->next = nullptr;
-      cur = next;
+      if (count < k)
+        break;
+      ListNode *prev = curr;
+      ListNode *node = groupStart;
+      for (int i = 0; i < k; i++) {
+        ListNode *nextNode = node->next;
+        node->next = prev;
+        prev = node;
+        node = nextNode;
+      }
+      preGroupEnd->next = prev;
+      preGroupEnd = groupStart;
     }
-    return result;
+    ListNode *newHead = dummy->next;
+    delete dummy;
+    return newHead;
   }
 };
 ListNode *createByTail() {
@@ -55,24 +61,21 @@ ListNode *createByTail() {
   }
   return head;
 }
-void display(vector<ListNode *> lnVec) {
-  for (size_t i = 0; i < lnVec.size(); i++) {
-    ListNode *p;
-    p = lnVec[i];
-    cout << "head-->";
-    while (p != NULL) {
-      cout << p->val << "-->";
-      p = p->next;
-    }
-    cout << "tail\n";
+void displayLink(ListNode *head) {
+  ListNode *p;
+  p = head;
+  cout << "head-->";
+  while (p != NULL) {
+    cout << p->val << "-->";
+    p = p->next;
   }
+  cout << "tail\n";
 }
 int main() {
-  vector<int> G;
   int k;
   ListNode *head = createByTail();
   cin >> k;
-  vector<ListNode *> lnVec = Solution().splitListToParts(head, k);
-  display(lnVec);
+  head = Solution().reverseKGroup(head, k);
+  displayLink(head);
   return 0;
 }
