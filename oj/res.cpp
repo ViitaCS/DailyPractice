40fb88ec6c369760909c86a249251ce7b7e01b2d
Viita <ysy825149732@gmail.com>
2025-07-22 13:20:17 +0800

    2025.7.22

--- oj/res.cpp
+++ oj/res.cpp
@@ -2,80 +2,55 @@
 #include <vector>
 using namespace std;
 
-struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(NULL) {}
-  ListNode(int x) : val(x), next(NULL) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
 class Solution {
 public:
-  ListNode *reverseKGroup(ListNode *head, int k) {
-    if (k == 1 || !head)
-      return head;
-    ListNode *dummy = new ListNode(0);
-    dummy->next = head;
-    ListNode *preGroupEnd = dummy;
-    while (true) {
-      ListNode *curr = preGroupEnd->next;
-      ListNode *groupStart = curr;
-      int count = 0;
-      while (curr && count < k) {
-        curr = curr->next;
-        count++;
+  int trap(vector<int> &height) {
+    int n = height.size();
+    if (n < 3)
+      return 0;
+
+    int left = 0, right = n - 1;
+    int left_max = 0, right_max = 0;
+    int ans = 0;
+
+    while (left < right) 
+    {
+      if (height[left] < height[right]) 
+      {
+        if (height[left] >= left_max) 
+        {
+          left_max = height[left]; 
+        } 
+        else 
+        {
+          ans += left_max - height[left];
+        }
+        left++; 
       }
-      if (count < k)
-        break;
-      ListNode *prev = curr;
-      ListNode *node = groupStart;
-      for (int i = 0; i < k; i++) {
-        ListNode *nextNode = node->next;
-        node->next = prev;
-        prev = node;
-        node = nextNode;
+      else 
+      {
+        if (height[right] >= right_max) 
+        {
+          right_max = height[right]; 
+        } 
+        else 
+        {
+          ans += right_max - height[right]; 
+        }
+        right--; 
       }
-      preGroupEnd->next = prev;
-      preGroupEnd = groupStart;
     }
-    ListNode *newHead = dummy->next;
-    delete dummy;
-    return newHead;
+    return ans;
   }
 };
-ListNode *createByTail() {
-  ListNode *head;
-  ListNode *p1, *p2;
-  int n = 0, num;
-  int len;
-  cin >> len;
-  head = NULL;
-  while (n < len && cin >> num) {
-    p1 = new ListNode(num);
-    n = n + 1;
-    if (n == 1)
-      head = p1;
-    else
-      p2->next = p1;
-    p2 = p1;
-  }
-  return head;
-}
-void displayLink(ListNode *head) {
-  ListNode *p;
-  p = head;
-  cout << "head-->";
-  while (p != NULL) {
-    cout << p->val << "-->";
-    p = p->next;
-  }
-  cout << "tail\n";
-}
+
 int main() {
-  int k;
-  ListNode *head = createByTail();
-  cin >> k;
-  head = Solution().reverseKGroup(head, k);
-  displayLink(head);
+  int h;
+  vector<int> v;
+  while (cin >> h) {
+    v.push_back(h);
+  }
+  int res = Solution().trap(v);
+  cout << res << endl;
   return 0;
 }
