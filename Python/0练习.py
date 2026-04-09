f2318cd63a6f6be378b3ddf2280f194faef58a94
Viita <ysy825149732@gmail.com>
2026-04-09 18:15:46 +0800

    2026.4.9

---
+++ Python/0练习.py
@@ -0,0 +1,48 @@
+# """
+# 案例：定义汽车类及使用类中的成员
+# """
+
+
+# # 1.定义汽车类
+# class Car:
+#     # 属性
+
+#     # 行为
+#     def run(self):
+#         print(f'{self}汽车在跑!...')
+#     def work(self):
+#         print(f'我是work函数,我的self值是{self}')
+#         self.run()
+
+
+# # 2.创建对象
+# car1 = Car()
+# car1.work()
+
+# 案例：定义手机类，能开机关机拍照
+
+
+# class Phone:
+#     def open(self):
+#         print(f"{self}phone open")
+
+#     def close(self):
+#         print(f"{self}phone close")
+
+#     def photo(self):
+#         print(f"{self}phone photo")
+
+
+# p1 = Phone()
+# print(f"p1=={p1}")
+# p1.open()
+# p1.photo()
+# p1.close()
+# print("-" * 20)
+# p2 = Phone()
+# print(f"p2=={p2}")
+# p2.open()
+# p2.photo()
+# p2.close()
+
+
